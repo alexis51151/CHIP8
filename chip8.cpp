f6bf04051834af30e4cdff01126b917f2bc50d11
Alexis Le Glaunec <alexisdu51151@gmail.com>
2025-08-28 09:03:47 -0500

    refactor(chip8): added header file for class definition

--- chip8.cpp
+++ chip8.cpp
@@ -1,29 +1,13 @@
-#include <cstdint>  // type defs 
 #include <fstream>  // IO
 #include <iterator>
-#include <random>   // RN_REG
 #include <chrono>   // measure time 
 #include <cstring>  // memset
 
+#include "chip8.hpp"
 
-// Useful constants for CHIP8
-// 
-// Number of registers
-const uint32_t N_REG = 16;
-// Memory available (in bytes)
-const uint32_t MEM_B = 4096;
-// Number of input keys
-const uint32_t N_KEYS = 16;
-// Width of the display screen (in pixels)
-const uint32_t DISPLAY_WIDTH = 64;
-// Height of the display screen (in pixels)
-const uint32_t DISPLAY_HEIGHT = 32;
-// Start address of the available memory for ROMs
-const uint32_t START_ADDRESS = 0x200;
-// Size in memory of the font (16 character of 5 bytes)
-const uint32_t FONTSET_SIZE = 80;
+
 // Bytes for the fontset (each byte represents a row on display) 
-uint8_t fontset[FONTSET_SIZE] = {
+const uint8_t fontset[FONTSET_SIZE] = {
 	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
 	0x20, 0x60, 0x20, 0x20, 0x70, // 1
 	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -44,66 +28,6 @@ uint8_t fontset[FONTSET_SIZE] = {
 // Start address for the fontset (section 2 in memory)
 const uint32_t FONSET_START_ADDRESS = 0x50;
 
-class Chip8 {
-  public:
-    // 16 8-bit registers (V0 to VF)
-    uint8_t registers[N_REG]{};
-    // 4KB of memory
-    uint8_t memory[MEM_B]{};
-    // 16-bit index register (used for OPs)
-    uint16_t index{};
-    // 16-bit program counter
-    uint16_t pc{};
-    // stack of depth 16 (to store return address)
-    uint16_t stack[16]{};
-    // stack pointer
-    uint8_t sp{};
-    // 8-bit time clock
-    uint8_t delayTimer{};
-    // 8-bit sound clock
-    uint8_t soundTimer{};
-    // 16 input keys 
-    uint8_t keypad[N_KEYS]{};
-    // display screen
-    uint32_t video[DISPLAY_WIDTH * DISPLAY_HEIGHT]{};
-    // current OP code
-    uint16_t opcode;
-    // RNG
-    std::default_random_engine randGen;
-    // Randomly generated value
-    std::uniform_int_distribution<uint8_t> randBytes;
-  public:
-    Chip8();
-    void LoadROM(char const* filename);
-    // clear display
-    void OP_00E0();
-    // return from subroutine 
-    void OP_00EE();
-    // jump to nnn (lowest 12 bits of the instruction)
-    void OP_1nnn();
-    // call nnn (lowest 12 bits of the instruction)
-    void OP_2nnn();
-    // conditional eq skip (lowest byte of the instruction)
-    void OP_3xkk();
-    // conditional neq skip (lowest byte of the instruction)
-    void OP_4xkk();
-    // conditional skip with cmp between two registers
-    void OP_5xy0();
-    // load kk to register Vx
-    void OP_6xkk();
-    // add kk to register Vx
-    void OP_7xkk();
-    // load value of register Vy to register Vx
-    void OP_8xy0();
-    // bitwise or of Vx and Vy
-    void OP_8xy1();
-    // bitwise and of Vx and Vy
-    void OP_8xy2();
-    // XOR of Vx and Vy
-    void OP_8xy3();
-    // ADD of Vx and Vy in Vx with overflow flag
-    void OP_8xy4();
-};
 
 // Constructor: initialize the VM
 Chip8::Chip8() 
@@ -310,4 +234,73 @@ void Chip8::OP_8xy4() {
   // put the lowest 8 bits in Vx
   registers[Vx] = (sum & 0xFFu);
 }
+
+
+// '8xy5': SUB Vx, Vy
+void Chip8::OP_8xy5() {
+  uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+  uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+  // Set VF to carry (if overflow 8 bits)
+  registers[0xF] = (registers[Vx] > registers[Vy]);
+  
+  // put the lowest 8 bits in Vx
+  registers[Vx] -= registers[Vy];
+}
+
+
+// '8xy6': Set Vx = Vx SHR 1
+void Chip8::OP_8xy6() {
+  uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+  // Save LSB in VF
+  registers[0xF] = (registers[Vx] & 0x0001u);
+  
+  // Shift right by one
+  registers[Vx] >>= 1;
+}
+
+
+// '8xy7': SUBN Vx, Vy
+void Chip8::OP_8xy7() {
+  uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+  uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+  // Set VF to carry (if overflow 8 bits)
+  registers[0xF] = (registers[Vy] > registers[Vx]);
+  
+  // put the lowest 8 bits in Vx
+  registers[Vx] = registers[Vy] - registers[Vx];
+}
+
+
+
+// '8xyE': SHL Vx
+void Chip8::OP_8xyE() {
+  uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+  // Save MSB to VF
+  registers[0xF] = (registers[Vx] & 0x0080u);
+  
+  // Shift left by one
+  registers[Vx] <<= 1;
+}
+
+// '9xy0': SNE Vx, Vy
+// Conditional jump: skip next instruction if Vx != Vy
+void Chip8::OP_9xy0(){
+  uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+  uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+  // conditional jump
+  if (registers[Vx] != registers[Vy]) {
+    pc += 2;
+  }
+}
+
+
+
+
+
+
 
---
+++ chip8.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <cstdint> // type defs
+#include <random> // RNG1
+
+
+// Useful constants for CHIP8
+// 
+// Number of registers
+const uint32_t N_REG = 16;
+// Memory available (in bytes)
+const uint32_t MEM_B = 4096;
+// Number of input keys
+const uint32_t N_KEYS = 16;
+// Width of the display screen (in pixels)
+const uint32_t DISPLAY_WIDTH = 64;
+// Height of the display screen (in pixels)
+const uint32_t DISPLAY_HEIGHT = 32;
+// Start address of the available memory for ROMs
+const uint32_t START_ADDRESS = 0x200;
+// Size in memory of the font (16 character of 5 bytes)
+const uint32_t FONTSET_SIZE = 80;
+
+class Chip8 {
+  public:
+    Chip8();
+    void LoadROM(char const* filename);
+
+  private:
+   // 16 8-bit registers (V0 to VF)
+    uint8_t registers[N_REG]{};
+    // 4KB of memory
+    uint8_t memory[MEM_B]{};
+    // 16-bit index register (used for OPs)
+    uint16_t index{};
+    // 16-bit program counter
+    uint16_t pc{};
+    // stack of depth 16 (to store return address)
+    uint16_t stack[16]{};
+    // stack pointer
+    uint8_t sp{};
+    // 8-bit time clock
+    uint8_t delayTimer{};
+    // 8-bit sound clock
+    uint8_t soundTimer{};
+    // 16 input keys 
+    uint8_t keypad[N_KEYS]{};
+    // display screen
+    uint32_t video[DISPLAY_WIDTH * DISPLAY_HEIGHT]{};
+    // current OP code
+    uint16_t opcode;
+    // RNG
+    std::default_random_engine randGen;
+    // Randomly generated value
+    std::uniform_int_distribution<uint8_t> randBytes;
+
+    // clear display
+    void OP_00E0();
+    // return from subroutine 
+    void OP_00EE();
+    // jump to nnn (lowest 12 bits of the instruction)
+    void OP_1nnn();
+    // call nnn (lowest 12 bits of the instruction)
+    void OP_2nnn();
+    // conditional eq skip (lowest byte of the instruction)
+    void OP_3xkk();
+    // conditional neq skip (lowest byte of the instruction)
+    void OP_4xkk();
+    // conditional skip with cmp between two registers
+    void OP_5xy0();
+    // load kk to register Vx
+    void OP_6xkk();
+    // add kk to register Vx
+    void OP_7xkk();
+    // load value of register Vy to register Vx
+    void OP_8xy0();
+    // bitwise or of Vx and Vy
+    void OP_8xy1();
+    // bitwise and of Vx and Vy
+    void OP_8xy2();
+    // XOR of Vx and Vy
+    void OP_8xy3();
+    // ADD of Vx and Vy in Vx with overflow flag
+    void OP_8xy4();
+    // SUB Vx, Vy
+    void OP_8xy5();
+    // set Vx = Vx SHR 1 
+    void OP_8xy6();
+    // SUBN Vx, Vy (opposite of SUB)
+    void OP_8xy7();
+    // SHL Vx
+    void OP_8xyE();
+    // SNE Vx, Vy (conditional jump)
+    void OP_9xy0();
+    // LD I, addr (put addr at index)
+    void OP_Annn();
+};
+
