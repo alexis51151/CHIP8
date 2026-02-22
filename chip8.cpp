483266c7c664de5719d8e92ed274acd96293a36d
Alexis Le Glaunec <alexisdu51151@gmail.com>
2026-02-22 10:17:58 -0600

    added display function

--- chip8.cpp
+++ chip8.cpp
@@ -1,38 +1,35 @@
-#include <fstream>  // IO
+#include <chrono>  // measure time
+#include <cstring> // memset
+#include <fstream> // IO
 #include <iterator>
-#include <chrono>   // measure time 
-#include <cstring>  // memset
 
 #include "chip8.hpp"
 
-
-// Bytes for the fontset (each byte represents a row on display) 
+// Bytes for the fontset (each byte represents a row on display)
 const uint8_t fontset[FONTSET_SIZE] = {
-	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-	0x20, 0x60, 0x20, 0x20, 0x70, // 1
-	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 // Start address for the fontset (section 2 in memory)
 const uint32_t FONSET_START_ADDRESS = 0x50;
 
-
 // Constructor: initialize the VM
-Chip8::Chip8() 
-  : randGen(std::chrono::system_clock::now().time_since_epoch().count())
-{
+Chip8::Chip8()
+    : randGen(std::chrono::system_clock::now().time_since_epoch().count()) {
   // Initialize PC to section 3 (start for ROM)
   pc = START_ADDRESS;
 
@@ -46,14 +43,14 @@ Chip8::Chip8()
 }
 
 // Load ROM into VM
-void Chip8::LoadROM(char const* filename) {
+void Chip8::LoadROM(char const *filename) {
   // Open file in binary mode and move fp to the end
   std::ifstream file(filename, std::ios::binary | std::ios::ate);
 
   if (file.is_open()) {
     // Get file size and allocate buffer
-    std::streampos size=  file.tellg();
-    char* buffer = new char[size];
+    std::streampos size = file.tellg();
+    char *buffer = new char[size];
 
     // Move fp to the beginning of file
     file.seekg(0, std::ios::beg);
@@ -73,22 +70,22 @@ void Chip8::LoadROM(char const* filename) {
 // Emulation of the instructions
 // Source: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#0.0
 
-// '00E0': CLS 
+// '00E0': CLS
 // Clear the display
 void Chip8::OP_00E0() {
   // reset the video buffer
   std::fill(std::begin(video), std::end(video), 0);
 }
 
-// '00EE': RET 
+// '00EE': RET
 // Return from subroutine
 void Chip8::OP_00EE() {
   sp--;
   pc = stack[sp];
 }
 
-// '1nnn': JP addr 
-// Jump to location nnn 
+// '1nnn': JP addr
+// Jump to location nnn
 // We use the mask 0x0FFFu to retrieve the lowest 12 bits of opcode.
 void Chip8::OP_1nnn() {
   uint16_t addr = opcode & 0x0FFFu;
@@ -96,14 +93,14 @@ void Chip8::OP_1nnn() {
   pc = addr;
 }
 
-// '2nnn': CALL addr 
+// '2nnn': CALL addr
 // Call subroutine at nnn (lowest 12 bits of the instruction)
 // We use the mask 0x0FFFu to retrieve the lowest 12 bits of opcode.
-// Remember that PC is incremented by 2. Therefore, PC points to the next 
+// Remember that PC is incremented by 2. Therefore, PC points to the next
 // instruction to execute after the call.
 void Chip8::OP_2nnn() {
   uint16_t addr = opcode & 0x0FFFu;
-  
+
   // remember where to return
   stack[sp] = pc;
   sp++;
@@ -115,14 +112,14 @@ void Chip8::OP_2nnn() {
 // Skip next instruction if Vx == kk, where Vx is a register
 // kk corresponds to the lowest 8 bits of opcode.
 // x corresponds to the lower 4 bits of the high byte of the opcode.
-// Conditional jump based on test 
+// Conditional jump based on test
 void Chip8::OP_3xkk() {
-  // take the lower 4 bits of the high byte 
+  // take the lower 4 bits of the high byte
   uint8_t Vx = (opcode & 0x0F00u) >> 8u;
-  // take the first 
-  uint8_t byte = opcode  & 0x00FFu;
+  // take the first
+  uint8_t byte = opcode & 0x00FFu;
 
-  // conditional jump if Vx == k 
+  // conditional jump if Vx == k
   if (registers[Vx] == byte)
     pc += 2;
 }
@@ -131,14 +128,14 @@ void Chip8::OP_3xkk() {
 // Skip next instruction if Vx != kk, where Vx is a register
 // kk corresponds to the lowest 8 bits of opcode.
 // x corresponds to the lower 4 bits of the high byte of the opcode.
-// Conditional jump based on test 
+// Conditional jump based on test
 void Chip8::OP_4xkk() {
   // take the lower 4 bits of the high byte
   uint8_t Vx = (opcode & 0x0F00u) >> 8u;
-  // take the first 
-  uint8_t byte = opcode  & 0x00FFu;
+  // take the first
+  uint8_t byte = opcode & 0x00FFu;
 
-  // conditional jump if Vx == k 
+  // conditional jump if Vx == k
   if (registers[Vx] != byte)
     pc += 2;
 }
@@ -174,12 +171,12 @@ void Chip8::OP_6xkk() {
 
 // '7xkk': ADD Vx, byte
 void Chip8::OP_7xkk() {
- // take the lowest 8 bits
+  // take the lowest 8 bits
   uint8_t byte = (opcode & 0x00FFu);
   // take the lower 4 bits of the high byte
   uint8_t Vx = (opcode & 0x0F00u) >> 8u;
 
-  // add value to register 
+  // add value to register
   registers[Vx] += byte;
 }
 
@@ -230,11 +227,10 @@ void Chip8::OP_8xy4() {
 
   // Set VF to carry (if overflow 8 bits)
   registers[0xF] = (sum > 0xFFu);
-  
+
   // put the lowest 8 bits in Vx
   registers[Vx] = (sum & 0xFFu);
 }
-
 
 // '8xy5': SUB Vx, Vy
 void Chip8::OP_8xy5() {
@@ -243,11 +239,10 @@ void Chip8::OP_8xy5() {
 
   // Set VF to carry (if overflow 8 bits)
   registers[0xF] = (registers[Vx] > registers[Vy]);
-  
+
   // put the lowest 8 bits in Vx
   registers[Vx] -= registers[Vy];
 }
-
 
 // '8xy6': Set Vx = Vx SHR 1
 void Chip8::OP_8xy6() {
@@ -255,11 +250,10 @@ void Chip8::OP_8xy6() {
 
   // Save LSB in VF
   registers[0xF] = (registers[Vx] & 0x0001u);
-  
+
   // Shift right by one
   registers[Vx] >>= 1;
 }
-
 
 // '8xy7': SUBN Vx, Vy
 void Chip8::OP_8xy7() {
@@ -268,12 +262,10 @@ void Chip8::OP_8xy7() {
 
   // Set VF to carry (if overflow 8 bits)
   registers[0xF] = (registers[Vy] > registers[Vx]);
-  
+
   // put the lowest 8 bits in Vx
   registers[Vx] = registers[Vy] - registers[Vx];
 }
-
-
 
 // '8xyE': SHL Vx
 void Chip8::OP_8xyE() {
@@ -281,14 +273,14 @@ void Chip8::OP_8xyE() {
 
   // Save MSB to VF
   registers[0xF] = (registers[Vx] & 0x0080u);
-  
+
   // Shift left by one
   registers[Vx] <<= 1;
 }
 
 // '9xy0': SNE Vx, Vy
 // Conditional jump: skip next instruction if Vx != Vy
-void Chip8::OP_9xy0(){
+void Chip8::OP_9xy0() {
   uint8_t Vx = (opcode & 0x0F00u) >> 8u;
   uint8_t Vy = (opcode & 0x00F0u) >> 4u;
 
@@ -314,7 +306,6 @@ void Chip8::OP_Bnnn() {
   pc = nnn + registers[0];
 }
 
-
 // 'Cxkk': RND Vx, byte
 // Set Vx = random byte AND kk
 void Chip8::OP_Cxkk() {
@@ -324,4 +315,35 @@ void Chip8::OP_Cxkk() {
   registers[Vx] = randByte(randGen) & kk;
 }
 
+// 'Dxyn': DRW Vx, Vy, nibble
+// Display n-byte sprite starting at memory location I at (Vx, Vy), set VF =
+// collision
+// A sprite is 8-pixel wide
+// Thus, there are 8 columns (width) to have 64 pixels width
+void Chip8::OP_Dxyn() {
+  uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+  uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+  uint8_t height = (opcode & 0x000Fu);
 
+  // Wrap if going beyond display boundaries
+  uint8_t xPos = registers[Vx] % DISPLAY_WIDTH;
+  uint8_t yPos = registers[Vy] % DISPLAY_HEIGHT;
+
+  registers[0xF] = 0;
+
+  for (uint8_t row = 0; row < height; row++) {
+    uint8_t spriteByte = memory[index + row];
+    for (uint8_t col = 0; col < 8; col++) {
+      uint8_t spritePixel = (spriteByte & (0x80u >> col));
+      uint32_t *videoPixel = &video[(yPos + row) * DISPLAY_WIDTH + xPos + col];
+      if (spritePixel) {
+        // collision
+        if (*videoPixel == 0xFFFFFFFF) {
+          registers[0xF] = 1;
+        }
+        // videoPixel XOR spritePixel
+        *videoPixel ^= 0xFFFFFFFF;
+      }
+    }
+  }
+}
